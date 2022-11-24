7c65f9670ec689a0f28e9b9f6c91ef5eb72989e3
Evelyn Mendes <evlymn@gmail.com>
2022-11-24 00:57:51 -0300

    configurações iniciais - angular material e componentes login e home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", loadChildren: () => import('./login/login-routing.module').then(m => m.LoginRoutingModule)
+  },
+  {
+    path: "home", loadChildren: () => import('./home/home-routing.module').then(m => m.HomeRoutingModule)
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
