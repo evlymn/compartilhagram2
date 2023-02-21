b9c2367dfdcb12b2b934077f7c988f1f51b2a076
Evelyn Mendes <evlymn@gmail.com>
2023-02-21 19:31:14 -0300

    setas para visualizações da postagem

--- src/app/timeline/timeline-post/timeline-post.component.ts
+++ src/app/timeline/timeline-post/timeline-post.component.ts
@@ -72,4 +72,8 @@ export class TimelinePostComponent implements OnInit, AfterViewInit {
     this.totalComments = event.totalComments;
     this.userCommented = event.userCommented;
   }
+
+  goBack() {
+    history.back();
+  }
 }
